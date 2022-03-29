31cce902ece0178e22dfc594fea0a3224c856310
ThePandaJam <veronika.kravchenko@barbal.co>
2022-03-29 23:19:46 +0100

    code cleanup

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -8,19 +8,10 @@ public class Laser : MonoBehaviour
     [SerializeField]
     private float _speed = 8.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        //translate laser up
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
-        //if y position >8
-        //destroy object
         if (transform.position.y >= 8.0f)
         {
             Destroy(this.gameObject);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //public or private reference
-    //data type (int, float, bool, string)
-    //every variable has a name
-    //optional value assigned
     [SerializeField]
     private float _speed = 3.5f;
     [SerializeField]
     private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.15f;
+    private float _canFire = -1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        //take the current position = new position (0,0,0)
         transform.position = new Vector3(0, 0, 0);
     }
 
@@ -24,32 +22,34 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
-
-        //if user hits space key
-        //spawn gameObject
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
+            FireLaser();
+        }
+
+        void CalculateMovement()
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+            transform.Translate(direction * _speed * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+            if (transform.position.x >= 11.3f)
+            {
+                transform.position = new Vector3(-11.3f, transform.position.y, 0);
+            }
+            else if (transform.position.x <= -11.3f)
+            {
+                transform.position = new Vector3(11.3f, transform.position.y, 0);
+            }
+        }
+
+        void FireLaser()
+        {
+            _canFire = Time.time + _fireRate;
             Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
-        }
-    }
-
-    void CalculateMovement()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-
-        transform.Translate(direction * _speed * Time.deltaTime);
-        //clamping y position using Mathf.Clamp()
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
-
-        if (transform.position.x >= 11.3f)
-        {
-            transform.position = new Vector3(-11.3f, transform.position.y, 0);
-        }
-        else if (transform.position.x <= -11.3f)
-        {
-            transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
     }
 }
