9d924ffd917ad61bdc015c99980c6a0ad84c622c
ThePandaJam <veronika.kravchenko@barbal.co>
2022-03-29 16:44:26 +0100

    code cleanup and y position clamping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,30 +21,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(direction * _speed * Time.deltaTime);
+        //clamping y position using Mathf.Clamp()
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
 
-        //if player position on the y is >0
-        // then y position = 0
-        //else if position on the y is < -3.8f
-        // then y position = -3.8f
-
-        if (transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        }
-        else if (transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-        }
-
-        //if player on x > 11
-        // x pos = -11
-        //else if player on the x < -11
-        // x pos = 11
         if (transform.position.x >= 11.3f)
         {
             transform.position = new Vector3(-11.3f, transform.position.y, 0);
