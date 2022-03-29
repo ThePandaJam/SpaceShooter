27c5d0689b03b1f65343842659fbb79f31fa842a
ThePandaJam <veronika.kravchenko@barbal.co>
2022-03-29 16:18:30 +0100

    replaced direction implementation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,10 +23,8 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
-        //new Vector3(1, 0, 0) * horizontal input value * 3.5f * real time
-        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
-
+        transform.Translate(direction * _speed * Time.deltaTime);
     }
 }
