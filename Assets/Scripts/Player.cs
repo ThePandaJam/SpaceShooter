1bbbb2a300d13935180f06de86f30aa6cc9f4c20
ThePandaJam <veronika.kravchenko@barbal.co>
2022-03-29 16:37:25 +0100

    added player position bounds on y and wraparound on x axis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -26,5 +26,32 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        //if player position on the y is >0
+        // then y position = 0
+        //else if position on the y is < -3.8f
+        // then y position = -3.8f
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        //if player on x > 11
+        // x pos = -11
+        //else if player on the x < -11
+        // x pos = 11
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
