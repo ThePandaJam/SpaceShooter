59d11db862c7c85c5937a7e129fb9c6d125248cf
ThePandaJam <veronika.kravchenko@barbal.co>
2022-04-05 18:14:00 +0100

    laser destroys asteroid with explosion

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -6,16 +6,28 @@ public class Asteroid : MonoBehaviour
 {
     [SerializeField]
     private float _rotationSpeed = 19.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject _explosionPrefab;
 
     // Update is called once per frame
     void Update()
     {
         //rotate the asteroid on Z axis
         transform.Rotate(Vector3.forward * _rotationSpeed * Time.deltaTime);
+    }
+
+    //check for laser collision (trigger)
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            //instantiate explosion at the position of the asteroid (this)
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject, 0.25f);
+
+        }
+
     }
 }
