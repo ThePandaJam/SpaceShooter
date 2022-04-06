c635c6d4affd9177c7cf4f17ace172afddf27fc0
ThePandaJam <veronika.kravchenko@barbal.co>
2022-04-06 10:52:51 +0100

    added explosion sound and logic

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+    [SerializeField]
+    private AudioSource _audioSource;
+    [SerializeField]
+    private AudioClip _explosionSound;
     // Start is called before the first frame update
     void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
+
+        if (_audioSource == null)
+        {
+            Debug.LogError("Audio Source on the Asteroid is NULL.");
+        }
+        else
+        {
+            _audioSource.clip = _explosionSound;
+        }
+
         Destroy(this.gameObject, 3.0f);
     }
 
